172af57a4b4412dcde918ba876115e73ad4ed84c
Kazkoo <lab@starphase.sk>
2024-05-05 03:44:24 +0200

    WIP Nesting Lists

--- src/components/PageLinks.jsx
+++ src/components/PageLinks.jsx
@@ -90,7 +90,7 @@ export const sections = [
             TestingOutcomesPage,
             SeedingOptionsPage,
             PickHistoryPage,
-            // NestingListsPage,
+            NestingListsPage,
             CustomizingListsPage,
             ChangelogPage
         ],
